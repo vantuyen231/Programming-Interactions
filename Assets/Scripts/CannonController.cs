fc7c9e922d23966c74f3f3c258ce40437bc6c323
vantuyen231 <dovantuyen21112002@gmail.com>
2026-04-27 01:02:52 +0700

    Cannon Controler

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -13,10 +13,18 @@ public class CannonController : MonoBehaviour
     [SerializeField] protected float aimMinRotation = -10f;
 
     [Header("TurnCannon")]
-    [SerializeField] protected Vector2 _turnDirection;
+    [SerializeField] protected Vector2 _turnDirection = Vector2.zero;
     [SerializeField] protected float turnSpeed = 30f;
     [SerializeField] protected float turnMaxRotation = 30f;
     [SerializeField] protected float turnMinRotation = -30f;
+
+    [Header("RollWheels")]
+    [SerializeField] protected Vector2 _wheelRotation = Vector2.zero;
+    [SerializeField] protected Transform wheelFrontRight;
+    [SerializeField] protected Transform wheelRearRight;
+    [SerializeField] protected Transform wheelFrontLeft;
+    [SerializeField] protected Transform wheelRearLeft;
+
 
 
 
@@ -61,8 +69,18 @@ public class CannonController : MonoBehaviour
         float turnAmout = _turnDirection.y + turnDirection * turnSpeed * Time.deltaTime;
         _turnDirection.y = Mathf.Clamp(turnAmout,turnMinRotation,turnMaxRotation);
         transform.localEulerAngles = _turnDirection;
+
+        _wheelRotation.x = _turnDirection.y;
+        wheelFrontLeft.localEulerAngles = _wheelRotation;
+        wheelFrontRight.localEulerAngles = -_wheelRotation;
+        wheelRearLeft.localEulerAngles = _wheelRotation;
+        wheelRearRight.localEulerAngles = -_wheelRotation;
     }
 
+    private void Roll(float roll)
+    {
+
+    }
     private void DoFire()
     {
         Debug.Log("fire");
